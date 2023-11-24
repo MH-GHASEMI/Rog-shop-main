d25c675de98ef420dcd1916ebdccf2ba2bf5da7e
MH-ghasemi <mhmmmdy2@gmail.com>
2023-11-24 17:19:14 +0330

    takmilshod

---
+++ Sayte Rog shop/kharid.js
@@ -0,0 +1,9 @@
+var plaus=1;
+function ziad(){
+ plaus++;
+ document.getElementById("putmeghdar").value=plaus;
+}
+function kam(){
+    plaus--;
+    document.getElementById("putmeghdar").value=plaus;
+   }
